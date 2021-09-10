e7435502ff341bb09b4b89cbbf172fce550eb17d
Türker ALAN <87363243+alanturker@users.noreply.github.com>
2021-09-10 11:19:13 +0300

    Second Commit

---
+++ ClassDemo/ClassDemo/Enemy.swift
@@ -0,0 +1,17 @@
+
+class Enemy {
+    var health = 100
+    var attackStrength = 10
+    
+    func move() {
+        print("Walk forwards.")
+    }
+    
+    func attack() {
+        print("Lands a hit, does \(attackStrength) damage")
+    }
+    
+    
+    
+}
+
---
+++ ClassDemo/ClassDemo/main.swift
@@ -0,0 +1,11 @@
+
+
+let skeleton = Enemy()
+
+let dragon = Dragon()
+
+dragon.talk(speech: "My teeths are sword.")
+dragon.move()
+dragon.attackStrength = 15
+dragon.attack()
+
